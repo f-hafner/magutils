8bcde33c544a76dca22ae6ad882f04cc2b793e84
fhafner <hafner.flavio@gmail.com>
2022-05-20 18:12:33 +0200

    add get_linked_graduates

--- R/setup_environment.R
+++ R/setup_environment.R
@@ -1,4 +1,17 @@
 
+# Define some variables used in functions below
+threshold_prob_female <- 0.8
+
+degree_year_start <- 1985
+degree_year_end <- 2005
+
+
+geemp_fields <- c("geology", "geography", "environmental science",
+                  "mathematics", "computer science", "engineering",
+                  "chemistry", "physics", "economics")
+lps_fields <- c("biology", "psychology", "sociology", "political science")
+
+
 
 #' Connect to the database
 #'
@@ -11,9 +24,62 @@
 connect_to_db <- function(db_file) {
   con <- DBI::dbConnect(RSQLite::SQLite(), db_file)
   cat("The database connection is: \n")
-  print(src_dbi(con))
+  print(dbplyr::src_dbi(con))
   return(con)
 }
+
+
+# TODO: how to have good examples that work to a db that lies elsewhere?
+  # add toy database in the /data/? also for tests?
+
+#' Load links between MAG and ProQuest
+#'
+#' @param conn A DBI connection.
+#' @param keep_unique If TRUE (the default), drops graduates that have multiple links to MAG.
+#'
+#' @return A lazy query of linked goid-AuthorId.
+#' @export
+#'
+#' @examples
+#'
+#' @importFrom rlang .data
+#' @importFrom magrittr %>%
+get_linked_graduates <- function(conn, keep_unique = TRUE) {
+
+  if (keep_unique) {
+    drop_links <- dplyr::tbl(conn, "current_links") %>%
+      dplyr::select(.data$AuthorId, .data$goid) %>%
+      dplyr::collect() %>%
+      dplyr::group_by(.data$goid) %>%
+      dplyr::mutate(n_links = dplyr::n()) %>%
+      dplyr::ungroup() %>%
+      dplyr::filter(.data$n_links > 1) %>%
+      dplyr::pull(.data$goid) %>%
+      unique()
+
+    where_stmt <- paste0(
+      "WHERE goid NOT IN (",
+      paste0(drop_links, collapse = ", "),
+      "AND link_score > 0.7"
+    )
+  } else {
+    where_stmt <- "WHERE links_score > 0.7"
+  }
+
+  query_links <- paste0(
+    "SELECT AuthorId, goid, link_score
+      FROM current_links ",
+    where_stmt)
+
+  links <- dplyr::tbl(conn, dbplyr::sql(links))
+
+  return(links)
+}
+
+
+
+
+
 
 
 
